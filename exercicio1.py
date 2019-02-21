763cb7533abc0a9c581fc366e93aa057001069c4
unknown <=>
2019-02-20 21:50:49 -0300

    Aula2 Python

---
+++ exercicio1.py
@@ -0,0 +1,8 @@
+import random
+
+lista = [random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100),random.randrange(1,100)]    
+maxlista = max(lista)
+
+print('A lista gerou os seguintes numeros aleatorios: ' + str(lista))
+print('O maior valor da lista é o: '+ str(maxlista))
+
